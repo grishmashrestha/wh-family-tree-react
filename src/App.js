efdf0f36953b7d8b2f456697eb59b1aba9f5171a
Grishma Shrestha <grishmashrestha@gmail.com>
2019-04-20 13:53:16 +0545

    Initialize connection with api server

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
-
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  
   state = {
     todos: [
       {
         id: 1,
         title: "Take out the trash",
-        completed: false
+        completed: true
       },
       {
         id: 2,
@@ -23,9 +24,31 @@ class App extends Component {
       }
     ]
   }
+
+  onChangeHandler=event=>{
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0,
+    })
+  }
+
+  onClickHandler = () => {
+    const data = new FormData()
+    data.append('file', this.state.selectedFile)
+    axios.get("http://localhost:4000/api/v1/users", data, { 
+      // receive two parameter endpoint url ,form data 
+      })
+      .then(res => { 
+        // then print response status
+        console.log(res.statusText)
+      })
+   }
+
   render() {
     return (
       <div className="App">
+        <input type="file" name="file" onChange={this.onChangeHandler}/>
+        <button type="button" className="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button> 
         <Todos todos={this.state.todos}/>
       </div>
     );
