d98637e23659b57fc67ffbbb5fca154c44291223
Grishma Shrestha <grishmashrestha@gmail.com>
2019-04-21 22:22:22 +0545

    Make it pretty

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
+import Header from './components/layout/Header'
 import axios from 'axios';
 import './App.css';
 
@@ -7,6 +8,11 @@ class App extends Component {
   
   state = {
     users: []
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:4000/api/v1/users")
+      .then(res => this.setState({ users: res.data.users }));
   }
 
   onChangeHandler=event=>{
@@ -28,11 +34,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <input type="file" name="file" onChange={this.onChangeHandler}/>
-        <button type="button" className="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button> 
+        <Header/>
+        <div style={UploadDivStyle}>
+          <label>Select file to upload </label>
+          <input type="file" name="file" onChange={this.onChangeHandler}/>
+          <button type="button" className="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>   
+        </div>
+        
         {/* <Todos todos={this.state.todos}/> */}
-        <div>
-          <ul>
+        <div style={UserListStyle}>
+          <ul style={UserListUlStyle}>
             {this.state.users.map(item => (
               <li key={item.id}>{item.full_name}, {item.address || "no address"}</li>
             ))}
@@ -44,4 +55,21 @@ class App extends Component {
   }
 }
 
+const UploadDivStyle = {
+  borderStyle: 'solid',
+  borderColor: '#333',
+  textAlign: 'center',
+  padding:  '10px'
+
+}
+
+const UserListStyle = {
+  padding:  '10px'
+}
+
+const UserListUlStyle = {
+  textAlign: 'left',
+  margin: 'auto',
+  width: '60%'
+}
 export default App;
